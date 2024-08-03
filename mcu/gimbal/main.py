a7f253142a6f0f754780ea6538ae3d85ee2901d0
dbinkv1 <dbinkv1@Gmail.com>
2024-08-03 10:51:51 +0800

    feat✨:  "walnut_gimbal": 更新了伺服控制、蓝牙和WiFi连接逻辑
    - 重构了servo.py中的Servo类，增加了更多配置选项，如频率、脉宽等，并实现了角度限制和复位功能。
    - 在main.py中引入了蓝牙模块和相应的回调处理，用于处理BLE通信，并能通过UART与上位机交互。
    - 更新了boot.py中的WiFi连接策略，简化了连接过程并注释了不再使用的连接选项。
    - 整体上，这些变更旨在优化飞行控制器的无线连接和伺服控制功能，为用户提供更稳定、灵活的操作体验。

--- mcu/gimbal/main.py
+++ mcu/gimbal/main.py
@@ -1,19 +1,46 @@
+import time
 from machine import UART
+
+import bluetooth,ble_simple_peripheral
 from servo import Servo
 
-uart = UART(1, 115200, rx=2, tx=1)  # 设置串口号1和波特率
+# 创建串口对象
+uart = UART(1, 115200, rx=21, tx=20)  # 设置串口号1和波特率
 uart.write("Hello 01Studio!")  # 发送一条数据
 
-servo_x = Servo(47)
-servo_y = Servo(48)
+# 创建BLE对象
+ble = bluetooth.BLE() # 构建BLE对象
+peer = ble_simple_peripheral.BLESimplePeripheral(ble,name='WalnutPi')
 
+def on_rx(msg):
+    global text
+
+    text = msg.decode("ascii")
+    
+    print("RX:",msg) #打印接收到的数据,数据格式为字节数组。
+    peer.send("I got: ") 
+    peer.send(text)
+    
+peer.on_write(on_rx) #从机接收回调函数，收到数据会进入on_rx函数。
+
+# 创建舵机对象
+servo_x = Servo(5)
+servo_x.set_limit(60,120)
+
+servo_y = Servo(6)
+servo_y.set_limit(60,120)
+
+# 创建缓冲区
 buffer = b""  # 创建一个缓冲区
 frame_header = b'['  # 定义帧头
 frame_footer = b']'  # 定义帧尾
 
+text = ''
+
 while True:
-    # 判断有无收到信息
+    # 判断有无收到信息    
     if uart.any():
+        
         text = uart.read(128)  # 接收128个字符
         buffer += text  # 将接收到的数据添加到缓冲区
         
--- mcu/gimbal/servo.py
+++ mcu/gimbal/servo.py
@@ -1,9 +1,20 @@
 from machine import PWM, Pin  # type: ignore
 import time
 
+
 class Servo:
     def __init__(
-        self, pin, freq=50, min_us=500, max_us=2500, max_angle=180, min_accu=0.3, targe_angle=90
+        self,
+        pin,
+        freq      = 50,         # 频率
+        min_us    = 500,        # 最小脉宽
+        max_us    = 2500,       # 最大脉宽         
+        max_angle = 180,        # 舵机可达最大角度
+        min_accu  = 0.3,        # 最小精度
+
+        targe_angle     = 90,   # 初始化目标角度
+        limit_min_angle = 0,    # 最小角度限制
+        limit_max_angle = 180   # 最大角度限制
     ):
         self.pin = pin
         self.pwm = PWM(Pin(pin), freq=freq, duty=0)
@@ -14,22 +25,29 @@ class Servo:
         self.max_angle = max_angle # 最大角度
         self.min_accu  = min_accu  # 最小精度
 
-        self.targe_angle = targe_angle # 初始化目标角度
+        self.limit_max_angle = limit_max_angle  # 最大角度限制
+        self.limit_min_angle = limit_min_angle  # 最小角度限制
+
+        self.targe_angle = targe_angle  # 初始化目标角度
         self.set_angle(targe_angle)
 
-    def set_angle(self, targe_angle):  #绝对角度运动
+    def set_limit(self, limit_min_angle, limit_max_angle):  # 限制角度
+        self.limit_max_angle = limit_max_angle
+        self.limit_min_angle = limit_min_angle
+
+    def set_angle(self, targe_angle):  # 绝对角度运动
 
         print(f"set_angle(): 传入舵机 {self.pin} 目标角度: {targe_angle}")
 
-        targe_angle = min(max(targe_angle, 0), self.max_angle)
-    
+        targe_angle = min(max(targe_angle, self.limit_min_angle), self.limit_max_angle) # 限制角度
+
         print(f"set_angle(): 实际舵机 {self.pin} 可达角度: {targe_angle}\n")
-        
+
         self.targe_angle = targe_angle
-    
+
         us = self.min_us + (self.max_us - self.min_us) * (targe_angle / self.max_angle)
         ns = int(us * 1000)
-        
+
         self.pwm.duty_ns(ns)
 
     def set_angle_relative(self, relative_angle):  # 相对角度运动
@@ -37,10 +55,13 @@ class Servo:
         self.targe_angle += relative_angle
         self.set_angle(self.targe_angle)
 
-    def step(self, step=1): # 以最小精度步进N步
+    def step(self, step=1):  # 以最小精度步进N步
         self.set_angle_relative(self.min_accu * step)
+        
+    def reset(self):  # 复位
+        self.set_angle(90)
 
-    def deinit(self): # 注销舵机
+    def deinit(self):  # 注销舵机
         self.pwm.deinit()
 
     def test_45(self):
@@ -61,7 +82,7 @@ class Servo:
         for i in range(0, 180, 1):
             self.set_angle(i)
             time.sleep(0.01)
-            
+
         for i in range(180, 0, -1):
             self.set_angle(i)
             time.sleep(0.01)
@@ -75,7 +96,7 @@ class Servo:
         self.set_angle_relative(-900)
         time.sleep(1)
 
-    def test_step(self):    
+    def test_step(self):
         print("\n步进转动测试\n")
         for i in range(0, 30):
             self.step(1)
@@ -84,38 +105,39 @@ class Servo:
     def test_scan(self):
 
         points = []
+
         def fun(KEY):
             points.append(int(self.targe_angle))
-            print(f'在 self.targe_angle 检测到 {points}')
+            print(f"在 self.targe_angle 检测到 {points}")
 
-        KEY=Pin(6,Pin.IN,Pin.PULL_UP) #构建KEY对象
-        KEY.irq(fun,Pin.IRQ_FALLING or Pin.IRQ_RISING) #定义中断，下降沿触发
-        
-        cnt = 0 
+        KEY = Pin(6, Pin.IN, Pin.PULL_UP)  # 构建KEY对象
+        KEY.irq(fun, Pin.IRQ_FALLING or Pin.IRQ_RISING)  # 定义中断，下降沿触发
+
+        cnt = 0
         while cnt < 50:
             self.step(-3)
             time.sleep(0.01)
             # print(self.targe_angle)
             cnt += 1
-            
+
         while True:
-            
-            cnt = 0 
+
+            cnt = 0
             while cnt < 100:
                 self.step(3)
                 time.sleep(0.01)
                 # print(self.targe_angle)
                 cnt += 1
-                
+
                 print(points)
-                
-            cnt = 0 
+
+            cnt = 0
             while cnt < 100:
                 self.step(-3)
                 time.sleep(0.01)
                 # print(self.targe_angle)
                 cnt += 1
-                
+
                 print(points)
 
 
@@ -124,17 +146,15 @@ if __name__ == "__main__":
     # servo_x = Servo(47)
     # servo_x.test_relative()
     # servo_x.test_step()
-    
-    servo_y = Servo(5)
-    #servo_y.test_relative()
-    # servo_y.test_step()
-    
-    time.sleep(1)
-    
-    servo_y.test_scan()
-    
-    time.sleep(1)
-    
-    servo_y.set_angle(90)
 
-    
+    servo_y = Servo(5)
+    # servo_y.test_relative()
+    # servo_y.test_step()
+
+    time.sleep(1)
+
+    servo_y.test_scan()
+
+    time.sleep(1)
+
+    servo_y.set_angle(90)
