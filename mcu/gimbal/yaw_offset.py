5e5f3621103efaaebcdf0cdb601c8db57bed03db
dbinkv1 <dbinkv1@Gmail.com>
2024-08-03 21:29:59 +0800

    feat✨: 尝试接入卡尔曼滤波器

--- mcu/gimbal/yaw_offset.py
+++ mcu/gimbal/yaw_offset.py
@@ -2,14 +2,20 @@ from machine import SoftI2C, Pin, UART, Timer
 import mpu6050
 import time
 import math
-from filter import window_filter
+from filter import window_filter, KalmanAngle
+
+# 创建 KalmanAngle 实例
+kalman_filter = KalmanAngle()
+
+# 设置初始角度（假设为0度）
+kalman_filter.setAngle(90)
 
 class MPU6050Controller:
     def __init__(self, scl_pin, sda_pin, 
                     uart_id   = 1, 
                     baud_rate = 115200, 
-                    rx_pin    = 12, 
-                    tx_pin    = 11, 
+                    rx_pin    = 21, 
+                    tx_pin    = 20, 
                     dt        = 0.02):
 
         self.i2c  = SoftI2C(scl=Pin(scl_pin), sda=Pin(sda_pin))
@@ -25,14 +31,16 @@ class MPU6050Controller:
         self.yaw_offset_values = []
         self.yaw_deg_values = []
         
-        self.fix_yaw_offset = -0.00277
+        self.fix_yaw_offset = 0.002219471      # 0.002219471 无滤波
         
         self.cnt = 0
-        self.test_time = 30
-        self.window_size = self.test_time / self.dt
+        self.test_time = 600
+        
+        self.dt2 = 0.02
+        
+        self.window_size = 1023 # self.test_time / self.dt2
         self.timer = None
 
-        self.dt2 = 0
 
     def test_yaw_offset(self):
         """
@@ -44,7 +52,7 @@ class MPU6050Controller:
 
         while True:
 
-            st = time.time()
+            st = time.ticks_us()
 
             gyro = self.mpu.get_values()['GyX'], self.mpu.get_values()['GyY'], self.mpu.get_values()['GyZ']
             
@@ -52,25 +60,31 @@ class MPU6050Controller:
             self.yaw_deg = self.yaw * 180 / math.pi
             
             self.yaw_offset = self.yaw - self.yaw_last
-            self.yaw_last   = self.yaw
+            
+            
             avg_yaw_offset  = window_filter(self.yaw_offset, self.yaw_offset_values, self.window_size)
             
-            self.yaw_deg   = window_filter(self.yaw_deg, self.yaw_deg_values, 3)
+            newRate = self.yaw_offset / self.dt2
+            # self.yaw_deg   = window_filter(self.yaw_deg, self.yaw_deg_values, 3)
+            self.yaw_deg   = kalman_filter.getAngle(self.yaw, newRate, self.dt2)
             
             self.cnt += 1
-            uart_msg = f"test:0,0,{self.yaw_deg},{avg_yaw_offset},{self.cnt}\n"
+            
+            uart_msg = f"yaw: {self.yaw_deg}, offset: {avg_yaw_offset}, cnt: {self.cnt}, delay: {self.dt2}\n"
             self.uart.write(uart_msg)
             print(uart_msg)
 
+            self.yaw_last   = self.yaw
+
             time.sleep(0.02)
 
-            et = time.time()
+            et = time.ticks_us()
 
-            self.dt2 = et - st
+            self.dt2 = (et - st)/1000000
             
-            if self.cnt > self.window_size:
-                print(f"测试完成, avg_yaw_offset = {avg_yaw_offset}")
-                break
+#             if self.cnt > self.window_size:
+#                 print(f"测试完成, avg_yaw_offset = {avg_yaw_offset}")
+#                 break
 
 
 if __name__ == "__main__":
@@ -80,5 +94,5 @@ if __name__ == "__main__":
 
     time.sleep(2)
     
-    controller = MPU6050Controller(2, 1)
+    controller = MPU6050Controller(0, 1)
     controller.test()
